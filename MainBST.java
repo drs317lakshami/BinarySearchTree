354a36eade6527b40742532c86573341b490302c
drs317lakshami <devrajkumar317@gmail.com>
2022-11-27 21:10:12 +0530

    Added: Checked if all are added with using size method in binary tree

---
+++ MainBST.java
@@ -0,0 +1,24 @@
+package com.bridgelabz.binarysearch;
+
+public class MainBST {
+
+	public static void main(String[] args) {
+		 BinarySearchTree tree = new BinarySearchTree();
+	        Node root = null;
+	        root = tree.insertData(root, 56);
+	        root = tree.insertData(root, 30);
+	        root = tree.insertData(root, 70);
+	        root = tree.insertData(root, 22);
+	        root = tree.insertData(root, 40);
+	        root = tree.insertData(root, 60);
+	        root = tree.insertData(root, 95);
+	        root = tree.insertData(root, 11);
+	        root = tree.insertData(root, 65);
+	        root = tree.insertData(root, 3);
+	        root = tree.insertData(root, 16);
+	        root = tree.insertData(root, 63);
+	        root = tree.insertData(root, 67);
+	        tree.printInorder(root);
+	        System.out.println();
+	        System.out.println("Number of Nodes in binary tree :" + tree.size());	      
+	    }}
